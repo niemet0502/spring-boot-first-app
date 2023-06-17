6c119008a6ae36eb1112c2cf8cb8ea0072e4b731
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-17 13:28:51 +0000

    first commit

---
+++ src/main/java/com/groupeisi/m2gl/mariusdemo/MariusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.mariusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/m2gl/mariusdemo/MariusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.mariusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
