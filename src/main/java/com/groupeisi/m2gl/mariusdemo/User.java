61e94c78d57b1ac8bcbde4a8d4c79f35f8fe557b
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-17 14:49:38 +0000

    created an user entity

---
+++ src/main/java/com/groupeisi/m2gl/mariusdemo/User.java
@@ -0,0 +1,19 @@
+package com.groupeisi.m2gl.mariusdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String firstName;
+
+    private  String lastName;
+
+    private int age;
+}
